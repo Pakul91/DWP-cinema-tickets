46405d6f9ceb9e0030e57b0d5b44b02cf8f76bb9
Tomasz Pakulniewicz <pakulniewicz.t@gmail.com>
2025-07-12 13:45:13 +0100

    calculate totals added to Ticket service

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -2,12 +2,21 @@ import TicketTypeRequest from "./lib/TicketTypeRequest.js";
 import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
 
 export default class TicketService {
+  #ticketPrices = {
+    INFANT: 0,
+    CHILD: 15,
+    ADULT: 25,
+  };
+
   purchaseTickets(accountId, ...ticketTypeRequests) {
     try {
       // Validate account ID
       this.#validateAccountId(accountId);
       // Validate ticket request
       this.#validateTicketRequests(ticketTypeRequests);
+      // Calculate totals
+      const totals = this.#calculateTotals(ticketTypeRequests);
+      // Validate order
     } catch (error) {
       throw new InvalidPurchaseException(
         `Failed to purchase tickets: ${error.message}`
@@ -28,11 +37,30 @@ export default class TicketService {
 
     for (const request of ticketTypeRequests) {
       if (!(request instanceof TicketTypeRequest)) {
-        // This will throw because 'request' is an array, not a TicketTypeRequest!
         throw new Error(
           "All ticket requests must be TicketTypeRequest instances"
         );
       }
     }
+  }
+
+  #calculateTotals(ticketTypeRequests) {
+    return ticketTypeRequests.reduce((summary, request) => {
+      const ticketType = request.getTicketType();
+      const noOfTickets = request.getNoOfTickets();
+
+      if (!summary[ticketType]) {
+        summary[ticketType] = {
+          quantity: 0,
+          totalPrice: 0,
+        };
+      }
+
+      summary[ticketType].quantity += noOfTickets;
+      summary[ticketType].totalPrice +=
+        noOfTickets * this.#ticketPrices[ticketType];
+
+      return summary;
+    }, {});
   }
 }
